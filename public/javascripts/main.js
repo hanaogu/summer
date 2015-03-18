a3da43a4cab4393e8b21a64c4e3703f6ee09bc59
sbabybird <sbabybird@gmail.com>
2015-03-18 16:45:17 +0800

    define Summer global var

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,48 @@
 Ext.BLANK_IMAGE_URL = 'images/white.gif'; //定义空白图片（防止没有联网，图标出不来）
 
+Summer = {}; //定义全局变量，承载全局函数
+
+var westPanel = Ext.create('Ext.tab.Panel', {
+  region: 'west',
+  collapsible: true,
+  width: 220,
+  split: true,
+  tabBarHeaderPosition: 1,
+  tabBar: {
+    flex: 20
+  },
+  items: [
+    Ext.create('Summer.view.Navigation', {
+      title: '功能导航',
+      loadurl: 'test/nav.json'
+    })
+  ]
+});
+
+var centerPanel = Ext.create('Ext.tab.Panel', {
+  region: 'center',
+  activeTab: 0,
+  items: [
+    {
+      title: 'Default Tab',
+      html: 'The first tab\'s content. Others may be added dynamically'
+    },
+    {
+      title: 'FormTag',
+      loader: {
+        url: 'KitchenSink/FormTag.js',
+        autoLoad: true,
+        renderer: 'component'
+      }
+    },
+    {
+      title: 'robot',
+      xtype: 'panel',
+      html: '<iframe width="100%" height="100%" src="test/robot.html"></iframe>',
+    }
+  ]
+});
+
 Ext.onReady(function () {
   Ext.create('Ext.container.Viewport', {
     layout: 'border',
@@ -7,14 +50,7 @@ Ext.onReady(function () {
       Ext.create('Summer.view.TitleBar', {
         region: 'north'
       }),
-      Ext.create('Summer.view.Navigation', {
-        region: 'west',
-        collapsible: true,
-        split: true,
-        width: 250,
-        title: '功能导航',
-        loadurl: 'test/nav.json'
-      }),
+      westPanel,
       {
         region: 'south',
         title: 'South Panel',
@@ -31,30 +67,7 @@ Ext.onReady(function () {
         split: true,
         width: 150
       },
-      {
-        region: 'center',
-        xtype: 'tabpanel',
-        activeTab: 0,
-        items: [
-          {
-            title: 'Default Tab',
-            html: 'The first tab\'s content. Others may be added dynamically'
-          },
-          {
-            title: 'FormTag',
-            loader: {
-              url: 'KitchenSink/FormTag.js',
-              autoLoad: true,
-              renderer: 'component'
-            }
-          },
-          {
-            title: 'robot',
-            xtype: 'panel',
-            html: '<iframe width="100%" height="100%" src="test/robot.html"></iframe>',
-          }
-        ]
-      }
+      centerPanel
     ]
   });
 });
