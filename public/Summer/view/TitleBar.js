ca63ea379e70ad9ad287300accc1f38bc76eb0ee
sbabybird <sbabybird@gmail.com>
2015-03-19 17:18:51 +0800

    add userinfo menu

--- public/Summer/view/TitleBar.js
+++ public/Summer/view/TitleBar.js
@@ -10,18 +10,18 @@ Ext.define('Summer.view.LogoTitle', {
   loader: {
     url: 'logo',
     autoLoad: true,
-    renderer: function(loader, response, active) {
+    renderer: function (loader, response, active) {
       var logo = Ext.JSON.decode(response.responseText);
       var img = Ext.create('Ext.Img', {
-          src: logo.url,
-          cls: 'logo',
-        });
+        src: logo.url,
+        cls: 'logo',
+      });
       loader.getTarget().add(img);
       var title = Ext.create('Ext.Component', {
-          html: logo.title,
-          cls: 'title',
-        });
-        loader.getTarget().add(title);
+        html: logo.title,
+        cls: 'title',
+      });
+      loader.getTarget().add(title);
       //loader.getTarget().getEl().dom.href = logo.href;
       return true;
     }
@@ -29,24 +29,53 @@ Ext.define('Summer.view.LogoTitle', {
 });
 
 Ext.define('Summer.view.UserInfo', {
-  extend: 'Ext.container.Container',
+  extend: 'Ext.button.Button',
   xtype: 'userinfo',
-  layout: 'column',
+  cls: 'user_info',
+  iconCls: 'user_menu',
+  scale: 'medium',
+  icon: 'images/user.jpg',
   loader: {
     url: 'user',
     autoLoad: true,
-    renderer: function(loader, response, active) {
+    renderer: function (loader, response, active) {
       var user = Ext.JSON.decode(response.responseText);
-      var img = Ext.create('Ext.Img', {
-          src: user.photo,
-          imgCls: 'userphoto'
-        });
-        loader.getTarget().add(img);
-        var username = Ext.create('Ext.Component', {
-          html: user.name,
-          cls: 'username'
-        });
-        loader.getTarget().add(username);
+      var me = loader.getTarget();
+      //me.setIcon(user.photo);
+      console.log(user.photo);
+      me.setText(user.name);
+      me.setMenu([Ext.create('Ext.menu.Menu', {
+        width: 128,
+        floating: false,
+        items: [
+          {
+            cls: 'menu_list',
+            text: '个人主页',
+          },
+          {
+            cls: 'menu_list',
+            icon: 'images/calendar.gif',
+            text: '修改密码',
+          },
+          {
+            cls: 'menu_list',
+            icon: 'images/edit.png',
+            text: '账号管理',
+          },
+          {
+            cls: 'menu_list',
+            icon: 'images/control_power_blue.png',
+            text: '退出系统',
+            handler: function () {
+              Ext.MessageBox.confirm('系统提示', '确定要退出吗？',
+                function (a) {
+                  if (a == 'yes') {
+                    alert('退出成功');
+                  }
+                });
+            }
+          }]
+      })], false);
       return true;
     }
   }
@@ -59,7 +88,7 @@ Ext.define('Summer.view.TitleBar', {
   region: 'north',
   border: 0,
   height: 42,
-  bodyStyle: 'background: #438eb9',
+  bodyStyle: 'background: #438EB9',
   items: [
     {
       xtype: 'logotitle',
@@ -67,7 +96,7 @@ Ext.define('Summer.view.TitleBar', {
     },
     {
       xtype: 'userinfo',
-      style: 'float: right; border: 0; padding: 0 42px 0 0;background: none; margin: 0 0 0 0'
+      style: 'float: right; border: 0'
     }
   ]
 });
