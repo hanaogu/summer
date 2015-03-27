5ad0ceab4ed30797c7860f9a7d9546adcc8bffe7
sbabybird <sbabybird@gmail.com>
2015-03-27 11:24:25 +0800

    update DataGrid

--- Summer/view/DataGrid.js
+++ Summer/view/DataGrid.js
@@ -3,7 +3,6 @@ Ext.define('Summer.view.DataGrid', {
   initComponent: function () {
     var me = this;
     this.initStore(me);
-    this.initToolbar(me);
     this.initPagingBar(me);
     this.callParent();
   },
@@ -23,50 +22,6 @@ Ext.define('Summer.view.DataGrid', {
         simpleSortMode: true
       },
     });
-  },
-  initToolbar: function(me) {
-    me.tbar = [
-      {
-        xtype: 'textfield',
-        emptyText: me.sdata.queryText,
-        itemId: 'queryfield'
-      },
-      {
-        text: '查询',
-        icon: 'images/zoom.png',
-        handler: function () {
-          var na = me.down('#queryfield').getValue();
-          if (na == null) na = '';
-          me.store.proxy.extraParams.query = na;
-          me.store.reload();
-        },
-      },
-      '->', {
-        text: '新增',
-        icon: 'images/add.png',
-        handler: function() {
-          me.onAdd(me);
-        },
-      },
-      '-', {
-        text: '编辑',
-        icon: 'images/edit.png',
-        disabled: true,
-        handler: function() {
-          me.onEdit(me);
-        },
-        itemId: 'edit',
-      },
-      '-', {
-        text: '删除',
-        icon: 'images/delete.png',
-        disabled: true,
-        handler: function() {
-          me.onDel(me);
-        },
-        itemId: 'del',
-      }
-    ];
   },
   initPagingBar: function (me) {
     me.bbar = Ext.create('Ext.PagingToolbar', {
